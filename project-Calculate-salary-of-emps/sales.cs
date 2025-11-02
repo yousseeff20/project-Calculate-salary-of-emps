a3b9398eb5a7ebdad139aebb3bca30e5fb94795f
yousef 2600052 <yousef2100175@fci.sohag.edu.eg>
2025-11-02 22:27:17 +0200

    add class sales

---
+++ project-Calculate-salary-of-emps/sales.cs
@@ -0,0 +1,30 @@
+﻿namespace project_Calculate_salary_of_emps
+{
+    public class sales : EMP
+    {
+        public const double commissionRate = 0.1;
+        public double salesAmount { get; set; }
+
+        public override double CalculateSalary()
+        {
+            return base.CalculateSalary() + (commissionRate * salesAmount);
+        }
+
+        private double CalculateCommission()
+        {
+            return commissionRate * salesAmount;
+        }
+
+        protected override double Calculate()
+        {
+            return base.CalculateSalary() + CalculateCommission();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                   $"\nCommission: ${Math.Round(CalculateCommission(), 2):N0}" +
+                   $"\nNet Salary: ${Math.Round(this.Calculate(), 2):N0}";
+        }
+    }
+}
